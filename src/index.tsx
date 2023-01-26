595589cbe7cb198b865b980f42f59131e2696435
Oksana <oksanaoksana25@gmail.com>
2023-01-26 13:22:31 +0200

    jsx rules

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,19 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 //jsx
-const h1 = <h1 id="title">Hello React.js</h1>
+const h1 = (
+    <h1 id="title" className="dfdf">
+        Hello React.js
+    </h1>
+)
 console.log(h1)
+
+let a = 10
+let b = `hello test`
 const list = (
     <ul>
-        <li>list Item 1</li>
-        <li>list Item 2</li>
+        <li>list Item {a + 10 + 10}</li>
+        <li>list Item {b}</li>
         <li>list Item 3</li>
     </ul>
 )
