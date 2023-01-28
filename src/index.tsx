186cc9f9b19dd5ca75c33fd9dcccd8e291d0bdb9
Oksana <oksanaoksana25@gmail.com>
2023-01-28 08:41:19 +0200

    imported component Title

--- src/index.tsx
+++ src/index.tsx
@@ -3,23 +3,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { isPropertySignature } from 'typescript'
 import { Content1 } from './Content'
-
-interface ITitleProps {
-    text: string
-}
-
-const Title = (props: ITitleProps) => {
-    console.log(props)
-
-    return <h1>Hello {props.text}</h1>
-}
+import Title from './Title'
 
 function App() {
     return (
         <>
             <Title text="React" />
             <Title text="TS" />
-            <Content
+            <Content1
                 text1={'Hello world 1'}
                 text2={'Hello world 2'}
                 year={2023}
