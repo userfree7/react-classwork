1238e61691f655ece8658db676465d3d055e9ef4
Oksana <oksanaoksana25@gmail.com>
2023-02-01 13:21:14 +0200

    add container into header and styled Header

--- src/container/Header/Header.tsx
+++ src/container/Header/Header.tsx
@@ -5,11 +5,12 @@ import Button from '@mui/material/Button'
 import IconButton from '@mui/material/IconButton'
 import MenuIcon from '@mui/icons-material/Menu'
 import Container from '@mui/material/Container'
+import './Header.scss'
 
 type Props = {}
 const Header = (props: Props) => {
     return (
-        <AppBar position="static">
+        <AppBar position="static" className="app-bar">
             <Container>
                 <Toolbar>
                     <IconButton
