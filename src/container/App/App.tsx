f6e8294f4a6fc2cc42f410a3fd06108521fbeb1e
Oksana <oksanaoksana25@gmail.com>
2023-02-01 12:50:25 +0200

    add Header and imported styledEngineProvider

--- src/container/App/App.tsx
+++ src/container/App/App.tsx
@@ -1,13 +1,18 @@
 import Footer from 'container/Footer/Footer'
 import Header from 'container/Header/Header'
 import Main from 'container/Main/Main'
+import CssBaseline from '@mui/material/CssBaseline'
+import { StyledEngineProvider } from '@mui/material/styles'
 type Props = {}
 const App = (props: Props) => {
     return (
         <>
-            <Header />
-            <Main />
-            <Footer />
+            <StyledEngineProvider injectFirst>
+                <CssBaseline />
+                <Header />
+                <Main />
+                <Footer />
+            </StyledEngineProvider>
         </>
     )
 }
