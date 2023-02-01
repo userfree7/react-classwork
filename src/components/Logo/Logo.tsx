2877edf68a0468db091427c859f77c1b6c0efcf8
Oksana <oksanaoksana25@gmail.com>
2023-02-01 17:44:02 +0200

    add Logo

---
+++ src/components/Logo/Logo.tsx
@@ -0,0 +1,13 @@
+import logo from 'assets/logo.svg'
+import Typography from '@mui/material/Typography'
+
+type Props = {}
+
+const Logo = (props: Props) => {
+    return (
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            <img src={logo} alt="Fake shop" />
+        </Typography>
+    )
+}
+export default Logo
--- src/container/Header/Header.tsx
+++ src/container/Header/Header.tsx
@@ -1,11 +1,12 @@
 import AppBar from '@mui/material/AppBar'
 import Toolbar from '@mui/material/Toolbar'
-import Typography from '@mui/material/Typography'
 import Button from '@mui/material/Button'
 import IconButton from '@mui/material/IconButton'
 import MenuIcon from '@mui/icons-material/Menu'
 import Container from '@mui/material/Container'
+
 import './Header.scss'
+import Logo from 'components/Logo/Logo'
 
 type Props = {}
 const Header = (props: Props) => {
@@ -22,13 +23,7 @@ const Header = (props: Props) => {
                     >
                         <MenuIcon />
                     </IconButton>
-                    <Typography
-                        variant="h6"
-                        component="div"
-                        sx={{ flexGrow: 1 }}
-                    >
-                        News
-                    </Typography>
+                    <Logo />
                     <Button color="inherit">Login</Button>
                 </Toolbar>
             </Container>
