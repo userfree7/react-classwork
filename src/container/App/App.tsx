d81df438fa9f4c2a1b5bc91f3c0a5e004496614c
Oksana <oksanaoksana25@gmail.com>
2023-02-24 21:22:04 +0200

    added removeProductFromCart, button Delete Product

--- src/container/App/App.tsx
+++ src/container/App/App.tsx
@@ -27,12 +27,22 @@ const App = (props: Props) => {
             [id]: (prevState[id] || 0) + count,
         }))
     }
+    const removeProductFromCart = (id: number) => {
+        setProductsInCart((prevState) => {
+            let prevProductsInCart = { ...prevState }
+            delete prevProductsInCart[id]
+            return prevProductsInCart
+        })
+    }
 
     return (
         <>
             <StyledEngineProvider injectFirst>
                 <CssBaseline />
                 <Header productsInCart={productsInCart} />
+                <button onClick={() => removeProductFromCart(1)}>
+                    Delete product
+                </button>
                 <Container
                     sx={{
                         padding: '60px 0',
